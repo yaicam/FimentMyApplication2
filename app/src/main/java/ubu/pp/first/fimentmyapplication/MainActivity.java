f97af7d106841a3ea03ac1b7b57c39b3d1052342
thanakorn yaicam <thanakorn.ya.58@ubu.ac.th>
2018-10-01 15:11:51 +0700

    Initial commit

---
+++ app/src/main/java/ubu/pp/first/fimentmyapplication/MainActivity.java
@@ -0,0 +1,60 @@
+package ubu.pp.first.fimentmyapplication;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    loadFragment(new HomeFragment());
+                    return true;
+
+                case R.id.navigation_dashboard:
+                    loadFragment(new DashboardFragment());
+                    return true;
+
+                case R.id.navigation_notifications:
+                    loadFragment(new NotificationsFragment());
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loadFragment(new HomeFragment());
+
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ app/src/main/java/ubu/pp/first/fimentmyapplication/NotificationsFragment.java
@@ -0,0 +1,16 @@
+package ubu.pp.first.fimentmyapplication;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NotificationsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_notifications, null);
+    }
+}
